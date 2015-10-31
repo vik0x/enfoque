a29a1111e05aca017a662fd4a87317cdd1c04a0a
Alan Sinue Pastor Piña <11460700@itcolima.edu.mx>
2015-10-31 14:43:11 -0600

    Se agregó la información del step al procedimiento almacenado "add_galeria"

--- DB.sql
+++ DB.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `enfoque` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `enfoque`;
 -- MySQL dump 10.13  Distrib 5.6.24, for Win32 (x86)
 --
 -- Host: localhost    Database: enfoque
@@ -35,11 +33,6 @@ CREATE TABLE elemento (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `elemento`
---
-
-
---
 -- Table structure for table `galeria`
 --
 
@@ -58,13 +51,8 @@ CREATE TABLE galeria (
   PRIMARY KEY (id_galeria),
   KEY fk_galeria_tipo_galeria_idx (id_tipo_galeria),
   CONSTRAINT fk_galeria_tipo_galeria FOREIGN KEY (id_tipo_galeria) REFERENCES tipo_galeria (id_tipo_galeria) ON DELETE NO ACTION ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `galeria`
---
-
 
 --
 -- Table structure for table `publicidad`
@@ -85,11 +73,6 @@ CREATE TABLE publicidad (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `publicidad`
---
-
-
---
 -- Table structure for table `tipo_galeria`
 --
 
@@ -101,13 +84,8 @@ CREATE TABLE tipo_galeria (
   nombre varchar(45) NOT NULL COMMENT 'El nombre del tipo de galería.',
   activo tinyint(1) NOT NULL DEFAULT '1' COMMENT 'Este campo sirve para poner en 1 cuando está activo y en 0 cuando no.',
   PRIMARY KEY (id_tipo_galeria)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='Esta tabla se engarga de almacenar los tipos de galerías existentes en el sistema, tienen nombre dependiendo el tipo de evento, ya sea de boda, xv años, etc.';
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='Esta tabla se engarga de almacenar los tipos de galerías existentes en el sistema, tienen nombre dependiendo el tipo de evento, ya sea de boda, xv años, etc.';
 /*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tipo_galeria`
---
-
 
 --
 -- Dumping events for database 'enfoque'
@@ -126,7 +104,7 @@ CREATE TABLE tipo_galeria (
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
-CREATE DEFINER=root@localhost PROCEDURE add_foto(nid_galeria int, nurl varchar(250) )
+CREATE PROCEDURE add_foto(nid_galeria int, nurl varchar(250) )
 BEGIN
 	insert into elemento (id_galeria, tipo_elemento, url) values(nid_galeria, 1, nurl);
 END ;;
@@ -145,11 +123,11 @@ DELIMITER ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
-CREATE DEFINER=root@localhost PROCEDURE add_galeria(nid_tipo_galeria int, nnombre varchar (45), descripcion varchar (45))
+CREATE PROCEDURE add_galeria(nid_tipo_galeria int, nnombre varchar (45), descripcion varchar (45))
 BEGIN
-	insert into galeria (id_tipo_galeria, nombre, descripcion, fecha_subida ) 
-    values (nid_tipo_galeria, nnombre, descripcion, now());
-    select max(id_galeria) from galeria;
+	insert into galeria (id_tipo_galeria, nombre, descripcion, fecha_subida, step ) 
+    values (nid_tipo_galeria, nnombre, descripcion, now(), 1 );
+    select max(id_galeria) from galeria as id_galeria;
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -166,7 +144,7 @@ DELIMITER ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
-CREATE DEFINER=root@localhost PROCEDURE add_portada(nid_galeria int, nportada varchar (250))
+CREATE PROCEDURE add_portada(nid_galeria int, nportada varchar (250))
 BEGIN
 	update galeria set portada=nportada, step=2, fecha_subida=now() where id_galeria=nid_galeria;
 END ;;
@@ -185,7 +163,7 @@ DELIMITER ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
-CREATE DEFINER=root@localhost PROCEDURE add_tipo_galeria(nnombre varchar (45))
+CREATE PROCEDURE add_tipo_galeria(nnombre varchar (45))
 BEGIN
 	insert into tipo_galeria (nombre) values (nnombre);
     
@@ -205,7 +183,7 @@ DELIMITER ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
-CREATE DEFINER=root@localhost PROCEDURE add_video(nid_galeria int, nurl varchar(250) )
+CREATE PROCEDURE add_video(nid_galeria int, nurl varchar(250) )
 BEGIN
 	insert into elemento (id_galeria, tipo_elemento, url) values(nid_galeria, 2, nurl);
 END ;;
@@ -224,7 +202,7 @@ DELIMITER ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
-CREATE DEFINER=root@localhost PROCEDURE del_tipo_galeria(nid int, nactivo int)
+CREATE PROCEDURE del_tipo_galeria(nid int, nactivo int)
 BEGIN
 	update tipo_galeria set activo=nactivo where id_tipo_galeria=nid;
 END ;;
@@ -243,7 +221,7 @@ DELIMITER ;
 /*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
 /*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
 DELIMITER ;;
-CREATE DEFINER=root@localhost PROCEDURE up_tipo_galeria(nid int, nnombre varchar (45))
+CREATE PROCEDURE up_tipo_galeria(nid int, nnombre varchar (45))
 BEGIN
 	update tipo_galeria set nombre=nnombre where id_tipo_galeria=nid;
 END ;;
